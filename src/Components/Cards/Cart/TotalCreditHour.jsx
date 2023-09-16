617aec6a9ce93e52e0de723e7f817faa7fc51b65
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2023-09-16 18:04:24 +0600

    credit hour section bug fixed

--- src/Components/Cards/Cart/TotalCreditHour.jsx
+++ src/Components/Cards/Cart/TotalCreditHour.jsx
@@ -10,14 +10,21 @@ const TotalCreditHour = ({selectData}) => {
     
     
     for(const data of selectData){
-       totalCredit += data.credit_hour;
+      
+       if(totalCredit < 20){
+        totalCredit += data.credit_hour;
+
+       }else{
+        toast('You do not have enough credit hour');
+
+       }
 
     }
 
     
     return (
         <div className="border-b-2 pb-3 border-gray-400">
-           <h3 className="text-2xl font-semibold my-3">Total Credit Hour: {`${totalCredit < 20 ? totalCredit : toast('You do not have enough credit hour') }`} </h3>
+           <h3 className="text-2xl font-semibold my-3">Total Credit Hour: {totalCredit} </h3>
            <ToastContainer></ToastContainer>
            
          </div>
