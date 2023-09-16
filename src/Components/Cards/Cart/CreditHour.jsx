f271c7c5f60b761888e6f2d1fc1f93bd1142d469
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2023-09-16 23:24:21 +0600

    fixed total credit hour and total remaining credit hour condition statement problem

--- src/Components/Cards/Cart/CreditHour.jsx
+++ src/Components/Cards/Cart/CreditHour.jsx
@@ -4,7 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 const CreditHour = ({selectData}) => {
     let remaining = 20;
     for(const data of selectData){
-        remaining -= parseInt(data.credit_hour);
+        remaining -= data.credit_hour;
         if(remaining < 0){
             toast('remaining hour cannot be less than zero')
         }
@@ -15,7 +15,7 @@ const CreditHour = ({selectData}) => {
 
     return (
         <div>
-             <h2 className="font-bold text-2xl mb-3 text-blue-600 pb-3 border-gray-400 border-b-2">Credit Hour Remaining {`${remaining >=0 ? remaining + "hr" : '0 hr'}`} </h2>
+             <h2 className="font-bold text-2xl mb-3 text-blue-600 pb-3 border-gray-400 border-b-2">Credit Hour Remaining {`${remaining >0 ? remaining + "hr" : '0 hr'}`} </h2>
              <ToastContainer/>
         </div>
     );
--- src/Components/Cards/Cart/TotalCreditHour.jsx
+++ src/Components/Cards/Cart/TotalCreditHour.jsx
@@ -2,17 +2,19 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PropTypes from 'prop-types';
 
-const TotalCreditHour = ({selectData}) => {
 
+const TotalCreditHour = ({selectData}) => {
+    
+    
+    
+    let value = 0; 
     let totalCredit = 0;
     
-
-    
-    
     for(const data of selectData){
-      
-       if(totalCredit < 20){
         totalCredit += data.credit_hour;
+       if(totalCredit <= 20){
+        value = totalCredit;
+        
 
        }else{
         toast('You do not have enough credit hour');
@@ -24,7 +26,7 @@ const TotalCreditHour = ({selectData}) => {
     
     return (
         <div className="border-b-2 pb-3 border-gray-400">
-           <h3 className="text-2xl font-semibold my-3">Total Credit Hour: {totalCredit} </h3>
+           <h3 className="text-2xl font-semibold my-3">Total Credit Hour: {value} </h3>
            <ToastContainer></ToastContainer>
            
          </div>
